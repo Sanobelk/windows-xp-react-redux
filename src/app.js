f266f1cc14ceb2360639e3870f7ae329d9967640
Sanobelk <63002703+Sanobelk@users.noreply.github.com>
2022-06-11 12:21:03 -0400

    reset state

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ class App extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            xp_loaded : true,
+            xp_loaded : false,
             userLoggedIn : false,
         }
         this.restartPC = this.restartPC.bind(this);
