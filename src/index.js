b00741462f1a54d3f463d5f696ed5471c80f796c
Sanobelk <63002703+Sanobelk@users.noreply.github.com>
2022-06-11 12:36:08 -0400

    another try at removing address bar

--- src/index.js
+++ src/index.js
@@ -7,13 +7,6 @@ import './index.css';
 import './winXP_startup.css';
 import './winXP_login.css';
 
-window.addEventListener("load",function() {
-    setTimeout(function(){
-        // This hides the address bar:
-        window.scrollTo(0, 1);
-    }, 0);
-});
-
 
 const ROOT = document.getElementById('root');
 ReactDOM.render(<App/>,ROOT);
