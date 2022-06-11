6a3dd48d5daca0891ea741b342adb58d3a9d1dc7
Sanobelk <63002703+Sanobelk@users.noreply.github.com>
2022-06-11 12:32:48 -0400

    added code to hide mobile address bar

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import './index.css';
 import './winXP_startup.css';
 import './winXP_login.css';
 
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
+});
+
 
 const ROOT = document.getElementById('root');
 ReactDOM.render(<App/>,ROOT);
