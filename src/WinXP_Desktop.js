e2acbcff734f697ea2c583ffd8d8f3e674425775
Sanobelk <63002703+Sanobelk@users.noreply.github.com>
2022-07-09 10:59:24 -0400

    fixed desktop clock by adding it to state.

--- src/WinXP_Desktop.js
+++ src/WinXP_Desktop.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 class WinXP_Desktop extends React.Component{
     constructor(props){
@@ -7,11 +7,17 @@ class WinXP_Desktop extends React.Component{
         this.state = {
             startMenu : false,
             startMenuClicked : false,
+            time : {
+                hour : null,
+                minute : null,
+                ampm : null,
+            }
         }
 
 
         this.toggleStartMenu = this.toggleStartMenu.bind(this);
         this.checkMenuOpen = this.checkMenuOpen.bind(this);
+        this.updateTime = this.updateTime.bind(this);
     }
 
     toggleStartMenu = () => { //toggles start menu
@@ -33,42 +39,70 @@ class WinXP_Desktop extends React.Component{
         }
     }
 
+    updateTime = () =>{
+        let date = new Date();
+        let hour;
+        let minute;
+        let ampm;
+        hour = date.getHours();
+        minute = date.getMinutes();
+        console.log(hour % 12);
+
+        /*SET AMPM*/
+        if(hour > 12){
+            ampm = 'PM'
+        }else{
+            ampm = 'AM'
+        }
+        /*CONVERT 24 HOUR INTO 12 HOUR TIME*/
+        hour = hour % 12 || 12;
+
+        /*SET THE HOUR*/
+        if(hour % 12 >= 0){
+            this.setState({
+                ...this.state,
+                time : {
+                    hour,
+                    minute,
+                    ampm,
+                }
+            })
+        }else if (hour % 12 < 0){
+            this.setState({
+                ...this.state,
+                time : {
+                    hour,
+                    minute,
+                    ampm,
+                }
+            })
+        }
+        /*SET THE MINUTES*/
+        if(minute > 10){
+            this.setState({
+                ...this.state,
+                time : {
+                    hour,
+                    minute,
+                    ampm,
+                }
+            })
+            console.log(this.state);
+        }
+    }
+
+    componentDidMount(){
+        setInterval(()=>{
+           this.updateTime(); 
+        },1000)
+    }
+
     render(){
 
-        /*CLOCK*/
-
-        //Maybe throw this into state.
-
-        let date = new Date();
-        let hour
-        let minute
         
-        setInterval((clock)=>{
-            hour = date.getHours();
-            hour = hour % 12 || 12;
-            minute = date.getMinutes();
-            if(minute > 10){
-                document.getElementById('timeClockMinute').textContent = minute;
-            }else if( minute <10){
-                document.getElementById('timeClockMinute').textContent = `0${minute}`
-            }
-            document.getElementById('timeClockHour').textContent = hour;
-            
-        },100);
-
-        /*     */
-        
-
-        /* immitation lag taskbar */
-
-        /*setTimeout(()=>{
-             document.querySelector('.DesktopArea').style.height="95%"; //because originally its 100%.
-        },3000)
-       */
 
 
         return (
-        
         <div className="WinXP_Desktop_Container">
             <div className="DesktopArea" onClick={this.checkMenuOpen}>
             
@@ -80,10 +114,10 @@ class WinXP_Desktop extends React.Component{
                 <div className="Taskbar--RightArea">
                 
                     <span id="timeClock">
-                        <span id="timeClockHour"></span> 
+                        <span id="timeClockHour">{this.state.time.hour}</span> 
                             : 
-                        <span id="timeClockMinute"></span>
-                        <span id="timeClockAMPM"></span>
+                        <span id="timeClockMinute">{this.state.time.minute} </span>
+                        <span id="timeClockAMPM">{this.state.time.ampm}</span>
                     </span>
                 
                 </div>
@@ -93,7 +127,7 @@ class WinXP_Desktop extends React.Component{
                         <div className="startMenu--Top">
                             <p className="startMenu--Top--UserName" onClick={this.props.restartPC}>Admin</p>
                         </div>
-                        <div className="startMenu--Middle">
+                        <div className="startMenu--Middle" onClick={this.updateTime}>
                             <div className="startMenu--Middle--Left">left</div>
                             <div className="startMenu--Middle--Right">right</div>
                         </div>
